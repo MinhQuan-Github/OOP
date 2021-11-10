8ebec9b630dafacf73abbd7c1b33e1b9c32f6e86
QuanDoCoder <dracolucius05032002@gmail.com>
2021-11-10 21:11:38 +0700

    adjust

--- Assignment7.1/Assignment7_1.cpp
+++ Assignment7.1/Assignment7_1.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 int main(){
     Vecto<int> a;
-    // cout<<"Nhập vecto a: "<<endl;
-    // cin>>a;
-    // Vecto<int> b;
-    // b = a; //b.operator = (a)
-    // cout<<"Vecto b:"<<b;
-    // cout<<"Nhập lại vecto b: "<<endl;
-    // cin>>b;
-    // c = a+b;
-    // cout<<"Vecto a:"<<a;
-    // cout<<"Vecto b:"<<b;
-    // cout<<"Vecto c=a+b:"<<c;
-    // c = a-b;
-    // cout<<"Vecto c=a-b:"<<c;
-    // cout<<"Tích vô hướng b * c = " << b*c << endl;
-    // cout<<"Phan tu thu 3 cua c: "<< c[2]<< endl;
+    cout<<"Nhập vecto a: "<<endl;
+    cin>>a;
+    Vecto<int> b;
+    b = a; //b.operator = (a)
+    cout<<"Vecto b:"<<b;
+    cout<<"Nhập lại vecto b: "<<endl;
+    cin>>b;
+    Vecto<int> c = a+b;
+    cout<<"Vecto a:"<<a;
+    cout<<"Vecto b:"<<b;
+    cout<<"Vecto c=a+b:"<<c;
+    c = a-b;
+    cout<<"Vecto c=a-b:"<<c;
+    cout<<"Tích vô hướng b * c = " << b*c << endl;
+    cout<<"Phan tu thu 3 cua c: "<< c[2]<< endl;
     cout << "hi";
     return 0;
 }
